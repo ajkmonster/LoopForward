94ec40c01bfa339ac4caf3eeb5d08cbe44bccb81
ajkmonster <phimphachanh.momo.victor@gmail.com>
2019-01-30 13:32:03 -0500

    loop sum new code

--- src/LoopSum.java
+++ src/LoopSum.java
@@ -1,8 +1,16 @@
+import java.util.Scanner;
+
 public class LoopSum {
     public static void main(String[] args) {
         int i;
-        for (i=1;i<=10;i++){
-            sum = i ;
+        int sum=0;
+        Scanner key = new Scanner(System.in);
+        System.out.print("Please enter a number from 1-100: ");
+        i = key.nextInt();
+        for (int j=i;j>=1;j--){
+            sum = sum+ j;
+            }
+        System.out.println("Sum from " +i+ "-1 is: "+ sum);
         }
+
     }
-}
